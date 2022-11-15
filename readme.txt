cd777a98e00e2db45e5ce20aba2363c6b5c03935
costa-rica <nrodrig1@gmail.com>
2022-11-15 09:15:50 +0100

    added page with cdn and page with download i.e. no cdn

---
+++ readme.txt
@@ -0,0 +1,18 @@
+There are only 3 files necessary to have the data tables: 
+jquery.js datatables.min.css, and datatables.min.js
+
+
+For the datatables files:
+Step1: go here: https://datatables.net/download/
+Step2: navigate to download
+Step3: select packages -- I have done (Step1: DataTables; Step2: DataTable, jQuery 3; Step3:Responsive)
+Step4: copy cdn to header of html page
+
+In my table_cdn page I have also imported a jQuery cdn from jQuery releases.
+However, you don't need to if you include jquery in step2 of the datatables.net/download/.
+If you make different selections you can see the two urls for the cdns change 
+just a little bit to include jquery or any other options you select.
+
+
+
+There could be other ways to do this but I haven't checked
--- run.py
+++ run.py
@@ -14,6 +14,29 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/table_cdn", methods=["GET","POST"])
+def table_cdn():
+    USER_ID = 12
+    file_name = f'user{USER_ID}_df_browse_apple.pkl'
+    # file_path = os.path.join(config.DF_FILES_DIR, file_name)
+    file_path = os.path.join("/Users/nick/Documents/_databases/ws08/df_files", file_name)
+
+    print(os.path.abspath(file_path))
+
+    df = pd.read_pickle(file_path)
+    df['index'] = range(1,len(df)+1)
+
+    df_list_of_rw_dicts = df.to_dict('records')
+
+    col_names = ['index', 'Name','Type', 'Count', 'df_exists']
+    print(col_names)
+
+    
+
+
+    return render_template("table_cdn.html", df = df_list_of_rw_dicts, col_names=col_names)
+
+
 @app.route("/table", methods=["GET","POST"])
 def table():
     USER_ID = 12
@@ -35,6 +58,7 @@ def table():
 
 
     return render_template("table.html", df = df_list_of_rw_dicts, col_names=col_names)
+
 
 
 
